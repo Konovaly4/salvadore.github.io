880fd20f507a945c38aa82fac42d8f4901ff3a15
Maxim Konovalov <maxkonovalov82@yandex.ru>
2020-09-22 22:41:43 +0300

    some adaptive changes in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,16 +2,21 @@ import React from 'react';
 import './Header.css';
 import logoPath from '../../Images/logo.png';
 import logoIgPath from '../../Images/logo_ig.svg';
+import phoneLogoPath from '../../Images/phone.png';
 
 function Header() {
 
   return (
     <header className="header">
       <img className="header__logo" src={logoPath} alt="logo" />
+      <button className="header__nav-button" />
       <p className="header__text header__text_worktime">10:00 - 21:00</p>
-      <p className="header__text header__text_address">ул. Воронцовская, д.19А, стр.1</p>
-      <p className="header__phone">8 903 100-48-20</p>
-      <a className="header__link" href="https://www.instagram.com/?hl=ru">
+      <div className="header__main-info">
+        <img className="header__phone-logo" src={phoneLogoPath} alt="phone-logo" />
+        <p className="header__text header__text_address">ул. Воронцовская, д.19А, стр.1</p>
+        <p className="header__phone">8 903 100-48-20</p>
+      </div>  
+      <a className="header__link" href="https://instagram.com/salon_salvadore?igshid=hbayq0uu6foi">
         <img src={logoIgPath} alt="logo-instagram" />
       </a>
     </header>
