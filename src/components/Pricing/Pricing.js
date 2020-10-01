bd4c0c021193f4fff29e7d4cba7ddb1bb2c8ab99
Konovaly4 <maxkonovalov82@yandex.ru>
2020-10-01 13:04:43 +0300

    slider code for Pricing block added but need to be fixed

--- src/components/Pricing/Pricing.js
+++ src/components/Pricing/Pricing.js
@@ -4,12 +4,43 @@ import SearchArrow from '../SearchArrow/SearchArrow';
 import './Pricing.css';
 
 const Pricing = (props) => {
+  let translateCounter = 0;
+  let minContainerWidth = 665;
+  let windowWidth = document.documentElement.clientWidth;
+
+  const rightSlider = (elem) => {
+    if (windowWidth >= (minContainerWidth/2)) {
+      elem.style.transform = `translateX(-${minContainerWidth - windowWidth}px)`;
+    } else  { if ((minContainerWidth - (translateCounter + windowWidth)) < windowWidth) {
+        elem.style.transform = `translateX(-${minContainerWidth - (translateCounter + windowWidth)}px)`;
+        translateCounter = translateCounter + (minContainerWidth - (translateCounter + windowWidth)) + windowWidth;
+      } else {
+        elem.style.transform = `translateX(-${windowWidth}px)`;
+        translateCounter = translateCounter + windowWidth
+      }
+    }
+  }
+
+  const leftSlider = (elem) => {
+    if (windowWidth >= (minContainerWidth/2)) {
+      elem.style.transform = `translateX(${minContainerWidth - windowWidth}px)`;
+    } else  { if ((minContainerWidth - (translateCounter + windowWidth)) < windowWidth) {
+        elem.style.transform = `translateX(${minContainerWidth - (translateCounter + windowWidth)}px)`;
+        translateCounter = translateCounter - (minContainerWidth - (translateCounter + windowWidth)) + windowWidth;
+      } else {
+        elem.style.transform = `translateX(${windowWidth}px)`;
+        translateCounter = translateCounter - windowWidth
+      }
+    }
+  }
+
+
   return (
-    <div className="pricing">
+    <section className="pricing">
       <header className="pricing__header">
-        <SearchArrow elemName="team__arrow" direction="left" active="true" />
+        <SearchArrow elemName="team__arrow" direction="left" active="true" onClick={leftSlider} />
         <h2 className="pricing__title">Услуги и цены</h2>
-        <SearchArrow elemName="team__arrow" direction="right" active="true" />
+        <SearchArrow elemName="team__arrow" direction="right" active="true" onClick={rightSlider} />
       </header>
       <ul className="pricing__container">
         {props.data.map((item, num) => (
@@ -19,7 +50,7 @@ const Pricing = (props) => {
             </li>
         ))}
       </ul>
-    </div>
+    </section>
   )
 }
 
