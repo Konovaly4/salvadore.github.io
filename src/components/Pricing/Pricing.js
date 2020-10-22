956956d14e984501e20293e616192e3ed6e6488f
Admin <maxkonovalov82@yandex.ru>
2020-10-22 16:55:08 +0300

    some modifications in transforming of Pricing block

--- src/components/Pricing/Pricing.js
+++ src/components/Pricing/Pricing.js
@@ -4,32 +4,31 @@ import SearchArrow from '../SearchArrow/SearchArrow';
 import './Pricing.css';
 
 const Pricing = (props) => {
-  let translateCounter = 0;
+  // let translateCounter = 0;
   let minContainerWidth = 665;
   let windowWidth = document.documentElement.clientWidth;
+  console.log(windowWidth)
+  const [transformation, setTransformation] = React.useState(0);
+  let containerStyle = `transform: translateX(-${transformation})`;
 
-  const rightSlider = (elem) => {
+  const rightSlider = () => {
     if (windowWidth >= (minContainerWidth/2)) {
-      elem.style.transform = `translateX(-${minContainerWidth - windowWidth}px)`;
-    } else  { if ((minContainerWidth - (translateCounter + windowWidth)) < windowWidth) {
-        elem.style.transform = `translateX(-${minContainerWidth - (translateCounter + windowWidth)}px)`;
-        translateCounter = translateCounter + (minContainerWidth - (translateCounter + windowWidth)) + windowWidth;
+      setTransformation(minContainerWidth - windowWidth);
+    } else  { if ((minContainerWidth - (transformation + windowWidth)) < windowWidth) {
+      setTransformation(minContainerWidth - (transformation + windowWidth));
       } else {
-        elem.style.transform = `translateX(-${windowWidth}px)`;
-        translateCounter = translateCounter + windowWidth
+        setTransformation(windowWidth);
       }
     }
   }
 
   const leftSlider = (elem) => {
     if (windowWidth >= (minContainerWidth/2)) {
-      elem.style.transform = `translateX(${minContainerWidth - windowWidth}px)`;
-    } else  { if ((minContainerWidth - (translateCounter + windowWidth)) < windowWidth) {
-        elem.style.transform = `translateX(${minContainerWidth - (translateCounter + windowWidth)}px)`;
-        translateCounter = translateCounter - (minContainerWidth - (translateCounter + windowWidth)) + windowWidth;
+      setTransformation(minContainerWidth - windowWidth);
+    } else  { if ((minContainerWidth - (transformation + windowWidth)) < windowWidth) {
+      setTransformation(minContainerWidth - (transformation + windowWidth));
       } else {
-        elem.style.transform = `translateX(${windowWidth}px)`;
-        translateCounter = translateCounter - windowWidth
+        setTransformation(windowWidth);
       }
     }
   }
@@ -42,7 +41,7 @@ const Pricing = (props) => {
         <h2 className="pricing__title">Услуги и цены</h2>
         <SearchArrow elemName="team__arrow" direction="right" active="true" onClick={rightSlider} />
       </header>
-      <ul className="pricing__container">
+      <ul className="pricing__container" style={{containerStyle}}>
         {props.data.map((item, num) => (
             <li key={num} className="pricing__service">
               <img className="prising__pic" alt={item.pictureAlt} src={item.src} />
